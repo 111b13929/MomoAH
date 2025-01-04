c69df472399bec3d791410911b6a7605693ccad6
jungchi <1111B13929@mailst.cjcu.edu.tw>
2025-01-04 17:13:21 +0800

    新增修改檔案

---
+++ MomoAH/Middlewares/GlobalExceptionMiddleware.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace MomoAH.Middlewares
+{
+    public class GlobalExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalExceptionMiddleware> _logger;
+
+        public GlobalExceptionMiddleware(RequestDelegate next, ILogger<GlobalExceptionMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "全域例外捕捉：{Message}", ex.Message); // 記錄例外
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = new
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "伺服器發生錯誤，請聯絡管理員。",
+                Details = context.Request.Host.Value.Contains("localhost") ? exception.StackTrace : null
+            };
+
+            return context.Response.WriteAsJsonAsync(response);
+        }
+
+
+    }
+}
